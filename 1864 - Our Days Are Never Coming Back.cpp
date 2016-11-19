5d4e137b9e676f3d8089d2012a9cf57266a0e520
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 11:31:26 +0600

    Create      1864 - Our Days Are Never Coming Back.cpp

---
+++ 1864 - Our Days Are Never Coming Back.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char s[] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+int n;
+
+int main(){
+	scanf("%d",&n);
+	for(int i=0; i<n; i++) printf("%c",s[i]);
+	printf("\n");
+	return 0;
+}
