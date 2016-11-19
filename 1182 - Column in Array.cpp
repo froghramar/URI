396e1dc3bdf21f2d41619e255e55907e11abf29a
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 11:06:47 +0600

    Create 1182 - Column in Array.cpp

---
+++ 1182 - Column in Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int l;
+char t;
+double soma,N[12][12];
+
+int main(){
+    scanf("%d %c ",&l,&t);
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf",&N[i][j]);
+            if(j == l) soma += N[i][j];
+        }
+    }   
+    if(t == 'S') printf("%.1f\n",soma);
+    else printf("%.1f\n", soma/12.0);
+    return 0;
+}
