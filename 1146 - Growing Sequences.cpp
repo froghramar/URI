b7eaa9e17529b62316a2184164074ac0ff3ab17b
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 16:30:03 +0600

    Create 1146 - Growing Sequences.cpp

---
+++ 1146 - Growing Sequences.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+	while(scanf("%d",&n) and n){
+		for(int i=1; i<n; i++) printf("%d ",i);
+		printf("%d\n",n);
+	}
+	return 0;
+}
