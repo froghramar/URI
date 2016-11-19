ecf1ab5e690eeb52a328f85626873edcc2488043
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 10:58:35 +0600

    Create 1564 - Brazil World Cup.cpp

---
+++ 1564 - Brazil World Cup.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+	while(scanf("%d",&n)==1){
+		if(n) printf("vai ter duas!\n");
+		else printf("vai ter copa!\n");
+	}
+	return 0;
+}
