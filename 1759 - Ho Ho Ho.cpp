9557f4578588c74da25e48ff4beca88057b7460c
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 11:34:15 +0600

    Create 1759 - Ho Ho Ho.cpp

---
+++ 1759 - Ho Ho Ho.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(){
+	scanf("%d",&n);
+	for(int i=1; i<n; i++) printf("Ho ");
+	if(n) printf("Ho!");
+	printf("\n");
+	return 0;
+}
