59f85f383d00c6ff0f819eace63c63ed8942f196
Feroz Ahmmed <froghramar@gmail.com>
2016-11-19 11:04:22 +0600

    Create 1188 - Inferior Area.cpp

---
+++ 1188 - Inferior Area.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int l;
+char t;
+double soma,N[12][12];
+
+int main(){
+    scanf(" %c ",&t);
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf",&N[i][j]);
+            if(j > (11 - i) && j < i) soma += N[i][j];
+        }
+    }   
+    if(t == 'S') printf("%.1f\n",soma);
+    else printf("%.1f\n", soma/66.0);
+    return 0;
+}
